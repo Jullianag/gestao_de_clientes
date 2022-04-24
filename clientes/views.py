373beea582cf649fc98e78494a8edcca7ce99575
jullianag <juh_g@hotmail.com>
2022-04-24 10:12:32 -0300

    corrigindo login

--- clientes/views.py
+++ clientes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from .models import Person
 from .forms import PersonForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def persons_list(request):
     persons = Person.objects.all()  # buscar todos os dados do banco
     return render(request, 'person.html', {'persons': persons})
 
 
+@login_required
 def persons_new(request):
     form = PersonForm(request.POST or None, request.FILES or None)   #  se tem algo no POST ele manda preenchido, se não é vazio
 
@@ -18,6 +20,7 @@ def persons_new(request):
     return render(request, 'person_form.html', {'form': form})
 
 
+@login_required
 def persons_update(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonForm(request.POST or None, request.FILES or None, instance=person)
@@ -28,6 +31,7 @@ def persons_update(request, id):
     return render(request, 'person_form.html', {'form': form})
 
 
+@login_required
 def persons_delete(request, id):
     person = get_object_or_404(Person, pk=id)
 
--- gestaodeclientes/settings.py
+++ gestaodeclientes/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'gestaodeclientes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,7 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 
 
 LOGIN_URL = '/login/'
